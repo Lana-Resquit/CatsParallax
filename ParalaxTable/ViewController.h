c4ad1abc734a965996b8f003e4a4388198718ba0
Lana-Resquit <kristy@Mac-Pro-stillHitch.local>
2015-03-25 12:39:43 -0700

    Initial Commit

---
+++ ParalaxTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParalaxTable
+//
+//  Created by Kristy on 25.03.15.
+//  Copyright (c) 2015 Kristyna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParalaxTable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParalaxTable
+//
+//  Created by Kristy on 25.03.15.
+//  Copyright (c) 2015 Kristyna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
