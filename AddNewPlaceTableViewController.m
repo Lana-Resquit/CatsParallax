c80ffe520d55e44439d08a409ca78795a7893b9e
Kristy <Kristy@hitchs-Mac-Pro.local>
2015-06-19 13:29:41 +0300

    fixed the summaryTextView in AddNewPlaceTableViewController

--- AddNewPlaceTableViewController.m
+++ AddNewPlaceTableViewController.m
@@ -22,6 +22,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.summaryTextView.delegate = self;
+    self.summaryTextView.text = @"Введите описание";
+    self.summaryTextView.textColor = [UIColor lightGrayColor];
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
